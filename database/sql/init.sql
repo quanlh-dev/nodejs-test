ae5b40faff867e831c0bbad5a2e5f9dd5025ab94
lehaiquantb <lehaiquantb@gmail.com>
2024-09-11 00:35:15 +0700

    chore

--- database/sql/init.sql
+++ database/sql/init.sql
@@ -7,12 +7,6 @@ CREATE TYPE FieldType AS ENUM(
 
 CREATE TABLE fields (
     id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    "createdAt" TIME WITHOUT TIME ZONE DEFAULT CURRENT_TIME,
-    "updatedAt" TIME WITHOUT TIME ZONE,
-    "deletedAt" TIME WITHOUT TIME ZONE,
-    "createdBy" INT,
-    "updatedBy" INT,
-    "deletedBy" INT,
     "name" VARCHAR(255) NOT NULL,
     "type" FieldType NOT NULL,
     "offsetFrom" INT NOT NULL DEFAULT 0,
